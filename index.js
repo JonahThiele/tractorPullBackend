849307586d09300f4093beef50bb350950c3cf06
JonahThiele <53587938+JonahThiele@users.noreply.github.com>
2025-05-21 16:04:09 -0500

    updated start script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const sql = require('./sql.js')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const passport = require('passport')
 const session = require('express-session')
